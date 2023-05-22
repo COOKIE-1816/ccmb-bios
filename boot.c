1e578a6a527969b9ce3055a2eeb0c34b78bd3c55
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-05-22 02:24:32 +0200

    Add files via upload

--- boot.c
+++ boot.c
@@ -1,6 +1,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#include <stdio.h>
+#include <stdbool.h>
+
 #include "boot.h"
 #include "sdcard.h"
 #include "sram.h"
@@ -59,4 +62,114 @@ bool fileExist(const char* file) {
     }
 
     return false;
+}
+
+bool readBootlinkIni(char* bootlinkContent, uint16_t maxContentLength) {
+    uint32_t sectorNumber = 0;
+    uint8_t sectorData[512];
+    uint16_t sectorOffset = 0;
+
+    bool fileFound = false;
+
+    while (!fileFound) {
+        if (!sdcard_readSector(sectorNumber, sectorData))
+            return false;
+
+        if (sectorData[0] == 0x00)
+            return false;
+
+        if (sectorData[0] != 0xE5 && (sectorData[11] & 0x08) == 0) {
+            char filename[13];
+
+            sdcard_currentFilename(sectorData, sectorOffset, filename);
+
+            if (strcmp(filename, "BOOTLINK.INI") == 0) {
+                uint16_t contentOffset = 0;
+                uint16_t contentLength = sectorData[28] + (sectorData[29] << 8);
+
+                while (contentLength > 0 && contentOffset < maxContentLength) {
+                    bootlinkContent[contentOffset++] = sectorData[32 + contentOffset];
+                    contentLength--;
+                }
+
+                bootlinkContent[contentOffset] = '\0';
+                fileFound = true;
+            }
+        }
+
+        sectorOffset += 32;
+        if (sectorOffset >= 512) {
+            sectorNumber++;
+            sectorOffset = 0;
+        }
+    }
+
+    return true;
+}
+
+bool loadFile(const char* filename, uint8_t* buffer, size_t bufferSize) {
+    FILE* file = fopen(filename, "rb");
+
+    if (file == NULL)
+        return false;
+
+    size_t bytesRead = fread(buffer, sizeof(uint8_t), bufferSize, file);
+    fclose(file);
+
+    if (bytesRead == 0)
+        return false;
+
+    return true;
+}
+
+void jumpToEntryPoint(void* entryPoint) {
+    __builtin_avr_delay_cycles(0);
+    asm volatile("jmp %0" ::"r"(entryPoint));
+}
+
+bool _bootFromFile(const char* bootFilename) {
+    const size_t bufferSize = 4096;
+    uint8_t buffer[bufferSize];
+
+    if (!loadFile(bootFilename, buffer, bufferSize))
+        return false;
+
+    jumpToEntryPoint(buffer);
+    return true;
+}
+
+bool bootFromFile() {
+    char bootlinkContent[MAX_BOOTLINK_SIZE];
+
+    if (readBootlinkIni(bootlinkContent, MAX_BOOTLINK_SIZE)) {
+        char bootFilename[32];
+        char* newlinePos = strchr(bootlinkContent, '\n');
+
+        if (newlinePos != NULL) {
+            size_t bootFilenameLength = newlinePos - bootlinkContent;
+
+            strncpy(bootFilename, bootlinkContent, bootFilenameLength);
+            bootFilename[bootFilenameLength] = '\0';  // Null-terminate the bootFilename string
+        } else {
+            // Handle the error appropriately
+        }
+
+        _bootFromFile(bootFilename);
+        return true;
+    }
+}
+
+bool bootFromDefault() {
+    _bootFromFile("BOOT.BIN")
+    return true;
+}
+
+void boot() {
+    if (bootFromFile())
+        return;
+
+    if (bootFromDefault())
+        return;
+
+    // TODO: Handle the error
 }
